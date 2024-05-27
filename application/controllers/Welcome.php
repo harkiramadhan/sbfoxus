7c62a6e1523a6adb96681516b265c1d850d6097a
harkiramadhan <harkiramadhan@gmail.com>
2024-05-27 20:57:58 +0700

    updates

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -24,4 +24,49 @@ class Welcome extends CI_Controller {
 			'messages' => 'API Online'
 		]));
 	}
+
+	function login(){
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://myaccount.foxus.com.au/login',
+			CURLOPT_HEADER => true,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS => http_build_query(array(
+				'username' => $this->input->post('username'),
+				'password' => $this->input->post('password')
+			)),
+			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_SSL_VERIFYHOST => false,
+		));
+
+		$response = curl_exec($curl);
+
+		if ($response === false) {
+			$error = curl_error($curl);
+			curl_close($curl);
+			die("cURL Error: $error");
+		}
+
+		$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+		$header = substr($response, 0, $header_size);
+		$body = substr($response, $header_size);
+
+		curl_close($curl);
+
+		preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $header, $matches);
+		$cookies = array();
+		foreach($matches[1] as $item) {
+			parse_str($item, $cookie);
+			$cookies = array_merge($cookies, $cookie);
+		}
+
+		$this->output->set_content_type('application/json')->set_output(json_encode($cookies));
+	}
 }
